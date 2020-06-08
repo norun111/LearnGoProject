9d2f3cfd29f244a01645fdbc78e9954dbb83bc01
tomoyaueno29 <u.t012807@gmail.com>
2020-06-08 16:20:54 +0900

    Create main.go

---
+++ data/main.go
@@ -0,0 +1,21 @@
+package main
+
+type Post struct {
+	Id int
+	Content string
+	Author string
+}
+
+var PostById map[int]*Post
+var PostsByAuthor map[string][]*Post
+
+func store(post Post) {
+
+	PostById[post.Id] = &post
+	PostsByAuthor[post.Author] = append(PostsByAuthor[post.Author], &post)
+}
+
+func main() {
+
+	PostById = make(map[int]*Post)
+}
