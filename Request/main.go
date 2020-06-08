bda049601d9802574d1573fabdbbe93a8e47a69e
tomoyaueno29 <u.t012807@gmail.com>
2020-06-08 14:03:49 +0900

    Update main.go

--- Request/main.go
+++ Request/main.go
@@ -14,7 +14,7 @@ func body(w http.ResponseWriter, r *http.Request){
 	len := r.ContentLength
 	body := make([]byte, len)
 	r.Body.Read(body)
-	fmt.Fprintln(w, string(body))
+	fmt.Fprintln(w, string(body ))
 }
 
 func main() {
