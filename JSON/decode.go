686f6d971ce0772ed2565e4fec7b8e4b39122405
tomoyaueno29 <u.t012807@gmail.com>
2020-06-09 16:17:59 +0900

    Create decode.go

---
+++ JSON/decode.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Post struct {
+	Id int `json:"id"`
+	Content string	`json:"content"`
+	Author Author	`json:"author"`
+	Comments []Comment `json:"comments"`
+}
+
+type Author struct {
+	Id int `jsomn:"id"`
+	Name string `json:"name"`
+}
+
+type Comment struct {
+	Id int `json:"id"`
+	Content string `json:"content"`
+	Author string `json:"author"`
+}
+
+func main() {
+
+	//jsonFile, _ := os.Open("post.json")
+	//decoder := json.NewDecoder(jsonFile)
+	//
+	//for {
+	//	var post Post
+	//	err := decoder.Decode(&post)
+	//	if err == io.EOF {
+	//		break
+	//	}
+	//	if err != nil {
+	//		fmt.Println("Error", err)
+	//		return
+	//	}
+	//	fmt.Println(post)
+	//}
+
+	jsonFile, _ := os.Open("post.json")
+
+	decoder := json.NewDecoder(jsonFile)
+
+	for{
+		var post Post
+		err := decoder.Decode(&post)
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(post)
+	}
+}
