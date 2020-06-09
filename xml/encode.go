1e89175c5503fcf5c0e728a132088f3a98afaf0c
tomoyaueno29 <u.t012807@gmail.com>
2020-06-09 15:27:17 +0900

    Create encode.go

---
+++ xml/encode.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"os"
+)
+
+type Post struct {
+	XMLName xml.Name `xml:"post"`
+	Id      string   `xml:"id,attr"`
+	Content string   `xml:"content"`
+	Author  Author   `xml:"author"`
+}
+
+type Author struct {
+	Id   string `xml:"id,attr"`
+	Name string `xml:",chardata"`
+}
+
+func main() {
+
+	post := Post{
+		Id: "1",
+		Content: "Hello World",
+		Author: Author{
+			Id: "2",
+			Name: "Sau Sheong",
+		},
+	}
+	xmlFile, _ := os.Create("post2.xml")
+
+	encoder := xml.NewEncoder(xmlFile)
+	encoder.Indent("", "\t")
+	err = encoder.Encode(&post)
+	if err != nil {
+		fmt.Println("Error encoding XML to file:", err)
+		return
+	}
+}
