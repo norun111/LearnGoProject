63e7fd7ae60c5d38938ff1795e389fccbae7e210
tomoyaueno29 <u.t012807@gmail.com>
2020-06-09 15:53:57 +0900

    Update main.go

--- JSON/main.go
+++ JSON/main.go
@@ -1,2 +1,19 @@
 package main
 
+type Post struct {
+	Id int `json:"id"`
+	Content string	`json:"content"`
+	Author Author	`json:"author"`
+	Comments []Comment `json:"comments"`
+}
+
+type Author struct {
+	Id string `jsomn:"id"`
+	Name string `json:"name"`
+}
+
+type Comment struct {
+	Id int `json:"id"`
+	Content string `json:"content"`
+	Author string `json:"author"`
+}
