b321842645de9e66bf4e987c2d3e7cfe7442fb2e
tomoyaueno29 <u.t012807@gmail.com>
2020-06-09 15:58:17 +0900

    Update main.go

--- JSON/main.go
+++ JSON/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
 type Post struct {
 	Id int `json:"id"`
 	Content string	`json:"content"`
@@ -16,4 +23,17 @@ type Comment struct {
 	Id int `json:"id"`
 	Content string `json:"content"`
 	Author string `json:"author"`
+}
+
+func main() {
+	jsonFile, err := os.Open("post.json")
+	if err != nil {
+		return
+	}
+	defer jsonFile.Close()
+	jsonData, _ := ioutil.ReadAll(jsonFile)
+
+	var post Post
+	json.Unmarshal(jsonData, &post)
+	fmt.Println(post)
 }
