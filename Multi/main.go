2846bea234e130c55f61c2d33ddc71e840fce09d
tomoyaueno29 <u.t012807@gmail.com>
2020-06-10 19:12:22 +0900

    Create main.go

---
+++ Multi/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	mux.HandleFunc("/", index)
+
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	server.ListenAndServe()
+}
