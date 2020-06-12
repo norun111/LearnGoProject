b961335f3a78a32ecf6529490cea4d45e0085dc4
tomoyaueno29 <u.t012807@gmail.com>
2020-06-12 18:01:45 +0900

    Update main.go

--- HTTP/main.go
+++ HTTP/main.go
@@ -29,7 +29,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 		fmt.Println("username:", r.Form["username"])
 		fmt.Println("password:", r.Form["password"])
-		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username"))) 
+		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username")))
 		fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
 		template.HTMLEscape(w, []byte(r.Form.Get("username")))
 	}
