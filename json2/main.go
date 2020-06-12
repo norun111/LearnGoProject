8dd780f974b0771a53ac30fadb692fa93921b2a8
tomoyaueno29 <u.t012807@gmail.com>
2020-06-12 21:06:26 +0900

    Update main.go

--- json2/main.go
+++ json2/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 )
@@ -33,12 +34,32 @@ func main() {
 		panic(err)
 	}
 	defer xmlFile.Close()
-	xmlData, err := ioutil.ReadAll(xmlFile)
-	if err != nil {
-		panic(err)
+	//xmlData, err := ioutil.ReadAll(xmlFile)
+	//if err != nil {
+	//	panic(err)
+	//}
+	decoder := xml.NewDecoder(xmlFile)
+	for {
+		t, err := decoder.Token()
+		if err == io.EOF{
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+
+		switch se := t.(type) {
+		case xml.StartElement:
+
+			if se.Name.Local == "comment" {
+				var comment Comment
+				decoder.DecodeElement(&comment, &se)
+			}
+		}
 	}
-	//fmt.Println(string(xmlData))
-	var post Post
-	xml.Unmarshal(xmlData, &post)
-	fmt.Println(post)
+
+	////fmt.Println(string(xmlData))
+	//var post Post
+	//xml.Unmarshal(xmlData, &post)
+	//fmt.Println(post)
 }
