93a5a90051de03f240b82e1e7072fd14e3005ccf
tomoyaueno29 <u.t012807@gmail.com>
2020-06-18 19:53:35 +0900

    Update main.go

--- goroutine/main.go
+++ goroutine/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func printNumbers1() {
 	for i := 0; i < 10; i++ {
+		fmt.Printf("%d", i)
+	}
+}
+
+func printNumbers2() {
+	for i := 0; i < 10; i++ {
+		time.Sleep(1 * time.Microsecond)
 		fmt.Printf("%d", i)
 	}
 }
@@ -12,6 +22,18 @@ func printLetters1() {
 	for i := 'A'; i < 'A'+10; i++ {
 		fmt.Printf("%c", i)
 	}
+}
+
+func printLetters2() {
+	for i := 'A'; i < 'A'+10; i++ {
+		time.Sleep(1 * time.Microsecond)
+		fmt.Printf("%c", i)
+	}
+}
+
+func goPrint2() {
+	go printNumbers2()
+	go printLetters2()
 }
 
 func print1() {
