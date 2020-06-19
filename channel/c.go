f50227b07c7f188d1d345ac61acf7d82373bee90
tomoyaueno29 <u.t012807@gmail.com>
2020-06-19 13:47:53 +0900

    Update c.go

--- channel/c.go
+++ channel/c.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	//ch := make(chan int)
+	//go func() {
+	//	ch <- 1
+	//}()
+	//a := <-ch
+	//fmt.Println(a)
+
+	ch := make(chan int, 5)
+
+	go func(){
+		for i := 1; i <= 10; i++ {
+			ch <- i
+		}
+	}()
+
+	time.Sleep(time.Second)
+
+	for i := 1; i <= 5; i++ {
+		tmp := <-ch
+		fmt.Println(tmp)
+	}
+
+	fmt.Println("witing")
+	time.Sleep(time.Second)
+
+	for i := 1; i <= 5; i++ {
+		tmp := <-ch
+		fmt.Println(tmp)
+	}
+}
