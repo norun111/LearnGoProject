c7cb07d4a2acb5974186278c39f9b3ff7564e630
tomoyaueno29 <u.t012807@gmail.com>
2020-06-24 16:17:57 +0900

    Create closer.go

---
+++ NM/closer.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fs := make([]func(), 3)
+
+	for i, _ := range fs {
+		fs[i] = func() { fmt.Println(i) }
+	}
+
+	for _, f := range fs {
+		f()
+	}
+}
