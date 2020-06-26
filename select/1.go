fcaccac346636e9cae19b096d1939201d5efb611
tomoyaueno29 <u.t012807@gmail.com>
2020-06-26 13:18:18 +0900

    Update 1.go

--- select/1.go
+++ select/1.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	c1 := make(chan interface{})
@@ -10,12 +13,14 @@ func main() {
 
 	var c1Count, c2Count int
 
-	for i := 0; i >= 0; i-- {
+	for i := 1000; i >= 0; i-- {
 		select {
 		case <-c1:
 			c1Count++
 		case <-c2:
 			c2Count++
+		case <-time.After(1 * time.Second):
+			fmt.Println("Time out")
 		}
 	}
 	fmt.Printf("c1Count: %d\nc2Count: %d\n", c1Count, c2Count)
