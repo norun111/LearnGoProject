0ecc2aac628ed994bb0d8b060ce7b6f97ba64529
tomoyaueno29 <u.t012807@gmail.com>
2020-06-26 13:28:07 +0900

    Create 2.go

---
+++ select/2.go
@@ -0,0 +1,33 @@
+package main
+
+import "time"
+
+func main() {
+	done := make(chan interface{})
+	go func() {
+		time.Sleep(5 * time.Second)
+		close(done)
+	}()
+
+	workCounter := 0
+loop:
+	for {
+		select {
+		case <-done:
+			break loop
+		default:
+		}
+
+		select {
+		case <- done:
+			break loop
+		default:
+		}
+
+		workCounter++
+		time.Sleep(1*time.Second)
+	}
+}
+
+		workCounter++
+		time.Sleep(1 * time.Second)
