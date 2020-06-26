1302b715b0da53b66a2194ebd5b80ada683134fb
tomoyaueno29 <u.t012807@gmail.com>
2020-06-26 19:16:53 +0900

    Update practice2.go

--- goroutine/practice2.go
+++ goroutine/practice2.go
@@ -14,21 +14,25 @@ func main() {
 
 func run(name ...string) {
 	fmt.Println("Start!")
+	// WaitGroupを作成する
 	wg := new(sync.WaitGroup)
-
+	// channelを処理の数分だけ作成する
 	isFin := make(chan bool, len(name))
 
 	for _, v := range name {
+		// 処理1つに対して、1つ数を増やす（この例の場合は5になる）
 		wg.Add(1)
+		// サブスレッドに処理を任せる
 		go process(v, isFin, wg)
 	}
-
+	// wg.Doneが5回通るまでブロックし続け　下は待て
 	wg.Wait()
 	close(isFin)
 	fmt.Println("Finish!")
 }
 
 func process(name string, isFin chan bool, wg *sync.WaitGroup) {
+	// wgの数を1つ減らす（この関数が終了した時）
 	defer wg.Done()
 	time.Sleep(2*time.Second)
 	fmt.Println(name)
